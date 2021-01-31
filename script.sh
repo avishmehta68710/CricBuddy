40851628287e0be0aa6eff61f11c2ec656d85076
avishmehta68710 <avishmehta68710@gmail.com>
2021-01-31 22:31:45 +0400

    Welcome To CricBuddy We Provide You regular updates of the cricket scoreboard

---
+++ script.sh
@@ -0,0 +1,1 @@
+while true; do echo -n "Current Time"; date ; sleep 2; python3 sc.py; done
---
+++ sheets.py
@@ -0,0 +1,14 @@
+import gspread
+from pprint import pprint
+from oauth2client.service_account import ServiceAccountCredentials
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("Cricket Alerts-a5d7fc29cc02.json",scope)
+client = gspread.authorize(creds)
+sheet = client.open("products").sheet1
+data = sheet.get_all_records()
+pprint(data)
+
+val = sheet.row_values(1)
+val1 = sheet.row_values(2)
+pprint(val)
+pprint(val1)

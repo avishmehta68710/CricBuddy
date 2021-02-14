fcb9f9f4ea194a01d698641c09c8d6cdf9cf9dd0
avishmehta68710 <avishmehta68710@gmail.com>
2021-02-14 09:18:27 +0400

    Cric Buddy

--- Cricket_Scores.py
+++ Cricket_Scores.py
@@ -46,21 +46,22 @@ for i in range(len(a)):
 
 # name of the team1
     team_1 = a[i].find("div",{"class":"teams"})
-    name_team1 = team_1.div.p.text
+    name_team1 = team_1.div.p.text.replace(",","|")
 # name of team2
     team_2 = (team_1.find_all("div",{"class":"team"}))
-    name_team2 = team_2[1].p.text
+    name_team2 = team_2[1].p.text.replace(",","|")
     print("Name of the teams "+name_team1+" VS "+name_team2)
     
     # batting indicator
     if status == "live":
         bb = a[i].find_all("div",{"class":"name-detail"})
+#        print(bb[0])
         try:
-            if bb[0].span.text == "":
-                batting_team = name_team1
+            if bb[0].span == "":
+                batting_team = name_team1.replace(",","|")
                 print("name of the batting team : "+name_team1.replace(",","|"))
         except:
-            batting_team = name_team2
+            batting_team = name_team2.replace(",","|")
             print("name of the batting team : "+name_team2.replace(",","|"))
     else:
         batting_team = "About to Begin"
@@ -70,18 +71,21 @@ for i in range(len(a)):
     if status == "result":
         c = (team_1.find("div",{"class":"score-detail"}))
         score_a = c.text
-        print("score of the "+name_team1+" is : "+c.text)
+        print("score of the "+name_team1+" is : "+c.text.replace(",","|"))
 
 # battting
         e = team_2[1].find("div",{"class":"score-detail"})
         d = e.find("span",{"class":"score"})
         score_b = d.text
-        print("score of the "+name_team2+" is : "+d.text)
+        print("score of the "+name_team2+" is : "+d.text.replace(",","|"))
         target = "Match Over"
 
     elif status == "live":
         # bowling score of team1
-        if bb[0].span.text == "":
+#        print(bb)
+#        print(bb[0])
+        if bb[0].span == "":
+#            print(bb[0])
             c = (team_1.find("div",{"class":"score-detail"}))
             score_a = (c.find("span",{"class":"score"}).text)
             print("score of the "+name_team1+" is : "+score_a)
@@ -89,23 +93,23 @@ for i in range(len(a)):
             d = team_2[0].find("div",{"class":"score-detail"})
 #                print("ddddd",team_2[0])
                 # target+overs
-            target = (d.find("span",{"class":"score-info"}).text)
+            target = (d.find("span",{"class":"score-info"}).text.replace(",","|"))
             print("target",target)
             print(" target to be acheived and overs completed "+target)
         else:
             d = team_2[1].find("div",{"class":"score-detail"})
             # target+overs
-            target = (d.find("span",{"class":"score-info"}).text)
+            target = (d.find("span",{"class":"score-info"}).text.replace(",","|"))
             print("target",target)
             print(" target to be acheived and overs completed "+target)
             # runs made
-            score_b = (d.find("span",{"class":"score"}).text)
+            score_b = (d.find("span",{"class":"score"}).text.replace(",","|"))
             print("score of the "+name_team2+" is : "+score_b)
 
 
 
     comment = (a[i].find("div",{"class":"status-text"}))
-    commentry = comment.span.text
+    commentry = comment.span.text.replace(",","|")
     print("match updates : "+commentry.strip())
 
     f.write(status+","+location+","+name_team1+","+name_team2+","+batting_team+","+score_a+","+score_b+","+target+","+commentry+"\n")
--- Scripts.sh
+++ Scripts.sh
@@ -1,1 +1,1 @@
-while true; do echo -n "Current Time"; date ; sleep 2; python3 sc.py; done
+while true; do echo -n "Current Time"; date ; sleep 2; python3 Cricket_Scores.py; done
